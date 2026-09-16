e4e503a7c3daff450ab36decd0325df84efdb8d1
JCS93 <192689701+j12Ch05@users.noreply.github.com>
2026-09-16 13:59:37 +0300

    Seeing the full content of the file

--- jc.c
+++ jc.c
@@ -1,15 +1,30 @@
+
+//test macros
+#define _DEFAULT_SOURCE
+#define _BSD_SOURCE
+#define _GNU_SOURCE 
+
+
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/ioctl.h>
+#include <sys/types.h>
 #include <termios.h>
 #include <unistd.h>
 
 #define JC_VERSION "0.0.1"
 #define CTRL_KEY(k) ((k) & 0x1f)
 #define ABUF_INIT {NULL,0}
+
+typedef struct erow
+{
+    int size;
+    char* chars;
+}erow;
+
 
 enum editorKey{
     ARROW_LEFT = 1000,
@@ -26,8 +41,11 @@ enum editorKey{
 struct editorConfig
 {
     int cx,cy;
+    int rowoff;
     int screenrows;
     int screencols;
+    int numrows;
+    erow *row;
     struct termios orig_termios;
 };
 
@@ -174,27 +192,74 @@ int getWindowSize(int *rows,int *cols){
     }
 }
 
+//row operations
+void editorAppendRow(char *s,ssize_t len){
+    E.row = realloc(E.row,sizeof(erow)*(E.numrows + 1));
+
+    int at = E.numrows;
+    E.row[at].size = len;
+    E.row[at].chars = malloc(len+1);
+    memcpy(E.row[at].chars,s,len);
+    E.row[at].chars[len] = '\0';
+    E.numrows++;
+}
+
+void editorOpen(char *filename){
+    FILE *fp = fopen(filename,"r");
+    if(!fp) die("open");
+
+    char *line = NULL;
+    ssize_t linecap = 0;
+    ssize_t linelen;
+   
+    while(( linelen = getline(&line,&linecap,fp)) != -1){
+        while(linelen > 0 && (line[linelen-1] == '\n' || line[linelen - 1] == '\r')){
+            linelen--;
+        }
+        editorAppendRow(line,linelen);
+    }
+    free(line);
+    fclose(fp);
+}
+
+void editorScroll(){
+    if(E.cy < E.rowoff){
+        E.rowoff = E.cy;
+    }
+    if(E.cy >= E.rowoff + E.screenrows){
+        E.rowoff = E.cy - E.screenrows+1;
+    }
+}
+
 //this function draws the ~ before each line like vim
 void editorDrawRows(struct abuf *ab){
     for(int y = 0 ; y < E.screenrows;y++){
-        if(y == E.screenrows / 3){
-            char welcome[80];
-            int welcomelen = snprintf(welcome,sizeof(welcome),"Just Code Editor -- version %s",JC_VERSION);
-            if(welcomelen > E.screencols) welcomelen = E.screencols;
-            int padding = (E.screencols - welcomelen) / 2;
-            if(padding){
+        int filerow = y + E.rowoff;
+        if(filerow >= E.numrows){
+                if(E.numrows==0 && y == E.screenrows / 3){
+                char welcome[80];
+                int welcomelen = snprintf(welcome,sizeof(welcome),"Just Code Editor -- version %s",JC_VERSION);
+                if(welcomelen > E.screencols) welcomelen = E.screencols;
+                int padding = (E.screencols - welcomelen) / 2;
+                if(padding){
+                    abAppend(ab,"~",1);
+                    padding--;
+                }
+                while (padding--)
+                {
+                    abAppend(ab," ",1);
+                }
+                
+                abAppend(ab,welcome,welcomelen);
+            }
+            else{
                 abAppend(ab,"~",1);
-                padding--;
             }
-            while (padding--)
-            {
-                abAppend(ab," ",1);
-            }
-            
-            abAppend(ab,welcome,welcomelen);
         }
         else{
-            abAppend(ab,"~",1);
+            int len = E.row[filerow].size;
+            if(len > E.screencols) len = E.screencols;
+            abAppend(ab,E.row[filerow].chars,len);
         }
 
         abAppend(ab, "\x1b[K", 3);
@@ -205,6 +270,8 @@ void editorDrawRows(struct abuf *ab){
 }
 
 void editorRefreshScreen(){ 
+    editorScroll();
+
     struct abuf ab = ABUF_INIT;
 
     abAppend(&ab, "\x1b[?25l", 6);// Adding this line stop the cursor from clitching
@@ -240,7 +307,7 @@ void editorMoveCursor(int key){
         break;
 
     case ARROW_DOWN:
-        if(E.cy != E.screenrows - 1){
+        if(E.cy < E.numrows){
             E.cy++;
         }
         break;
@@ -295,13 +362,19 @@ void editorProcessKeyPress(){
 
 void initEditor(){
     E.cx = E.cy = 0;
+    E.rowoff = 0;
+    E.numrows = 0;
+    E.row = NULL;
 
     if(getWindowSize(&E.screenrows,&E.screencols) == -1) die("getWindowSize");
 }
 
-int main(){
+int main(int argc,char *argv[]){
     enableRawMode();
     initEditor();
+    if(argc >=2){
+        editorOpen(argv[1]);
+    }
 
     while(1){
         editorRefreshScreen();
