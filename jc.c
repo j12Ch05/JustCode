f1e751a7e717f4c6500d5bc2825af965f543081d
JCS93 <192689701+j12Ch05@users.noreply.github.com>
2026-09-22 12:12:17 +0300

    Modifying message and unsaved changes quit

--- jc.c
+++ jc.c
@@ -20,6 +20,7 @@
 
 #define JC_VERSION "0.0.1"
 #define JC_TAB_STOP 8
+#define JC_QUIT_TIMES 3
 #define CTRL_KEY(k) ((k) & 0x1f)
 #define ABUF_INIT {NULL,0}
 
@@ -55,6 +56,7 @@ struct editorConfig
     int screencols;
     int numrows;
     erow *row;
+    int dirty;
     char *filename;
     char statusmsg[80];
     time_t statusmsg_time;
@@ -255,6 +257,7 @@ void editorAppendRow(char *s,ssize_t len){
     editorUpdateRow(&E.row[at]);
 
     E.numrows++;
+    E.dirty++;
 }
 
 void editorRowInsertChar(erow *row,int at,int c){
@@ -264,6 +267,7 @@ void editorRowInsertChar(erow *row,int at,int c){
     row->size++;
     row->chars[at] = c;
     editorUpdateRow(row);
+    E.dirty++;
 }
 
 // Editor Operations
@@ -313,6 +317,7 @@ void editorOpen(char *filename){
     }
     free(line);
     fclose(fp);
+    E.dirty = 0;
 }
 
 void editorSave(){
@@ -327,6 +332,7 @@ void editorSave(){
             if(write(fd,buf,len) == len){
                 close(fd);
                 free(buf);
+                E.dirty = 0;
                 editorSetStatusMessage("%d bytes written to disk", len);
                 return;
             }
@@ -399,7 +405,7 @@ void editorDrawRows(struct abuf *ab){
 void editorDrawStatusBar(struct abuf *ab){
     abAppend(ab,"\x1b[7m",4);
     char status[80],rstatus[80];
-    int len = snprintf(status,sizeof(status),"%.20s - %d lines",E.filename ? E.filename : "[No Name]",E.numrows);
+    int len = snprintf(status,sizeof(status),"%.20s - %d lines %s",E.filename ? E.filename : "[No Name]",E.numrows,E.dirty ? "(Modified)":"");
     int rlen = snprintf(rstatus,sizeof(rstatus),"%d/%d",E.cy + 1,E.numrows);
     if(len > E.screencols) len = E.screencols;
     abAppend(ab,status,len);
@@ -508,6 +514,8 @@ void editorMoveCursor(int key){
 
 //this function takes the user print (later it should print the user input)
 void editorProcessKeyPress(){
+    static int quit_times = JC_QUIT_TIMES;
+
     int c = editorReadKey();
 
     switch (c)
@@ -518,6 +526,11 @@ void editorProcessKeyPress(){
 
 
     case CTRL_KEY('l'):
+        if(E.dirty && quit_times > 0){
+            editorSetStatusMessage("WARNING!!! File has unsaved changes. Press CTRL-L %d more times to leave",quit_times);
+            quit_times--;
+            return;
+        }
         write(STDOUT_FILENO,"\x1b[2J",4);
         write(STDOUT_FILENO,"\x1b[H",3);
         exit(0);
@@ -577,7 +590,7 @@ void editorProcessKeyPress(){
         editorInsertChar(c);
         break;
     }
-
+    quit_times = JC_QUIT_TIMES;
 }
 
 void initEditor(){
@@ -587,6 +600,7 @@ void initEditor(){
     E.coloff = 0;
     E.numrows = 0;
     E.row = NULL;
+    E.dirty = 0;
     E.filename = NULL;
     E.statusmsg[0] = '\0';
     E.statusmsg_time = 0;
