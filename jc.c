08e5d0244e5d7f0a7c8be43c508b0639dc5d39c7
JCS93 <192689701+j12Ch05@users.noreply.github.com>
2026-09-23 20:54:50 +0300

    Adding the enter button function

--- jc.c
+++ jc.c
@@ -243,10 +243,12 @@ void editorUpdateRow(erow *row){
     row->rsize = idx;
 }
 
-void editorAppendRow(char *s,ssize_t len){
-    E.row = realloc(E.row,sizeof(erow)*(E.numrows + 1));
+void editorInsertRow(int at,char *s,ssize_t len){
+    if(at < 0  || at > E.numrows) return;
 
-    int at = E.numrows;
+    E.row = realloc(E.row,sizeof(erow)*(E.numrows + 1));
+    memmove(&E.row[at+1],&E.row[at],sizeof(erow) * (E.numrows - at));
+
     E.row[at].size = len;
     E.row[at].chars = malloc(len+1);
     memcpy(E.row[at].chars,s,len);
@@ -261,7 +263,7 @@ void editorAppendRow(char *s,ssize_t len){
 }
 
 void editorFreeRow(erow *row){
-    free(row->redners);
+    free(row->render);
     free(row->chars);
 }
 
@@ -303,10 +305,26 @@ void editorRowDelChar(erow *row,int at){
 // Editor Operations
 void editorInsertChar(int c){
     if(E.cy == E.numrows){
-        editorAppendRow("",0);
+        editorInsertRow(E.numrows,"",0);
     }
     editorRowInsertChar(&E.row[E.cy],E.cx,c);
     E.cx++;
+}
+
+void editorInsertNewLine(){
+    if(E.cx == 0){
+        editorInsertRow(E.cy,"",0);
+    }
+    else{
+        erow *row = &E.row[E.cy];
+        editorInsertRow(E.cy + 1,&row->chars[E.cx],row->size - E.cx);
+        row = &E.row[E.cy];
+        row->size = E.cx;
+        row->chars[row->size] = '\0';
+        editorUpdateRow(row);
+    }
+    E.cy++;
+    E.cx = 0;
 }
 
 void editorDelChar(){
@@ -360,7 +378,7 @@ void editorOpen(char *filename){
         while(linelen > 0 && (line[linelen-1] == '\n' || line[linelen - 1] == '\r')){
             linelen--;
         }
-        editorAppendRow(line,linelen);
+        editorInsertRow(E.numrows,line,linelen);
     }
     free(line);
     fclose(fp);
@@ -568,7 +586,7 @@ void editorProcessKeyPress(){
     switch (c)
     {
     case '\r':
-        /*TODO*/
+        editorInsertNewLine();
         break;
 
 
